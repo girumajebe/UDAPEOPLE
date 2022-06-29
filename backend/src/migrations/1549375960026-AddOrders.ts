760e3d06dc40f69144bf9f321ded1345f7671a3b
girumajebe <zr.one3@gmail.com>
2022-06-29 20:03:38 +0300

    migration

--- backend/src/migrations/1549375960026-AddOrders.ts
+++ backend/src/migrations/1549375960026-AddOrders.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner } from 'TYPEORM';
 
 export class AddOrders1549375960026 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
